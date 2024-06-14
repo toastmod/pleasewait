03c025ce2c8e046914a62fbe0fa93db25b7431e9
Andrew Toast <toastymod@gmail.com>
2024-06-14 14:47:58 -0600

    Use an outer type WaitLock for more options

--- src/waiter.rs
+++ src/waiter.rs
@@ -1,29 +1,51 @@
 use std::{sync::{atomic::{AtomicBool, Ordering}, Arc}, thread::Thread};
 
 pub struct Waiter {
-    parked: AtomicBool,
     thread: Thread
 }
 
 impl Waiter {
 
     pub fn wake(&self) {
-        if self.parked.load(Ordering::Acquire) {
-            self.parked.store(false, Ordering::Release);
-            self.thread.unpark();
-        }
+        self.thread.unpark();
     }
 
     pub fn wait(&self) {
-       self.parked.store(true, Ordering::Release); 
        std::thread::park();
     }
 
     pub fn create() -> Arc<Waiter> {
         Arc::new(
             Waiter {
-                parked: AtomicBool::new(false),
                 thread: std::thread::current()
+            }
+        ) 
+    }
+}
+
+pub struct WaitLock {
+    parked: AtomicBool,
+    waiter: Arc<Waiter>
+}
+
+impl WaitLock {
+    pub fn wake(&self) {
+        if self.parked.load(Ordering::Acquire) {
+            self.parked.store(false, Ordering::Release);
+            self.waiter.wake();
+        }
+    }
+
+    pub fn wait(&self) {
+       self.parked.store(true, Ordering::Release); 
+       self.waiter.wait();
+    }
+
+    pub fn create() -> Arc<WaitLock> {
+        Arc::new(
+            WaitLock {
+                parked: AtomicBool::new(false),
+                waiter: Waiter::create()
             }
         ) 
     }
